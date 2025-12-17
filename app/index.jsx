addc9a8517a613d73cc5641492c042bcaacffbe0
Adebayo Ayowale G. <haydexwisdom@gmail.com>
2025-12-17 16:11:50 +0100

    Completed the Welcome Screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,17 @@
 import { Image } from "expo-image";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import WelcomeImg from "@assets/images/welcome.webp";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeInDown, SlideInDown } from "react-native-reanimated";
+import Colors from "@constants/Colors";
+
+const AnimatedLinearGradient = Animated.createAnimatedComponent(LinearGradient);
+const AnimatedTouchableOpacity =
+  Animated.createAnimatedComponent(TouchableOpacity);
 
 const index = () => {
   return (
@@ -18,13 +24,49 @@ const index = () => {
         }}
       />
 
-      <LinearGradient
-        colors={["#4c669f", "#3b5998", "#192f6a"]}
+      <AnimatedLinearGradient
+        entering={FadeInDown.duration(500)}
+        colors={[
+          "rgba(255, 255, 255, 0)",
+
+          "rgba(255, 255, 255, 0.8)",
+          "rgba(255, 255, 255)",
+        ]}
         style={styles.container}
       >
-        <Text>Imagery</Text>
-        <Text>Every Image Tells a Story</Text>
-      </LinearGradient>
+        <Animated.Text
+          entering={FadeInDown.delay(200)
+            .springify()
+            .damping(12)
+            .stiffness(100)
+            .mass(1.5)}
+          style={styles.title}
+        >
+          Imagery
+        </Animated.Text>
+
+        <Animated.Text
+          entering={FadeInDown.delay(300)
+            .springify()
+            .damping(12)
+            .stiffness(100)
+            .mass(1.5)}
+          style={styles.subTitle}
+        >
+          Every Image Tells a Story
+        </Animated.Text>
+
+        <AnimatedTouchableOpacity
+          entering={FadeInDown.delay(400)
+            .springify()
+            .damping(12)
+            .stiffness(100)
+            .mass(1.5)}
+          style={styles.button}
+        >
+          <Text style={styles.buttonTitle}>Explore</Text>
+        </AnimatedTouchableOpacity>
+      </AnimatedLinearGradient>
     </View>
   );
 };
@@ -38,13 +80,34 @@ const styles = StyleSheet.create({
     bottom: 0,
     left: 0,
     height: hp("60%"),
-    paddingHorizontal: wp("5%"),
+    paddingHorizontal: wp("15%"),
     paddingBottom: hp("5%"),
     justifyContent: "flex-end",
     alignItems: "center",
+    gap: 8,
   },
-  title: {},
-  subTitle: {},
-  button: {},
-  buttonTitle: {},
+  title: {
+    fontSize: wp("15%"),
+    fontWeight: "500",
+    color: Colors.dark,
+  },
+  subTitle: {
+    fontSize: wp("4.5%"),
+    fontWeight: "500",
+    color: Colors.dark,
+  },
+  button: {
+    backgroundColor: Colors.dark,
+    width: "100%",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+    marginTop: 30,
+  },
+  buttonTitle: {
+    color: "white",
+    fontSize: wp("5%"),
+    fontWeight: "500",
+  },
 });
---
+++ constants/Colors.js
@@ -0,0 +1,12 @@
+const Colors = {
+  dark: "#000000df",
+  grey: "#64748B",
+  greyLight: "#CBD5E1",
+  background: "#FAFBFC",
+  surface: "#E2E8F0",
+  primary: "#2563EB",
+  secondary: "#7C3AED",
+  tint: "#0891B2",
+};
+
+export default Colors;
